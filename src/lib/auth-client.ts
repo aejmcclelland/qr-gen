1f43711ffbb8c76cd112c22bb8c62bb493a20da1
aejmcclelland <aejmcclelland@googlemail.com>
2025-12-25 23:10:20 +0000

    skip BaseUrl in betterauth

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -4,7 +4,7 @@ import { createAuthClient } from 'better-auth/react';
 
 export const authClient = createAuthClient({
 	// If you're on the same domain as the API, you can omit this or use NEXT_PUBLIC_BETTER_AUTH_URL
-	baseURL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL ?? 'http://localhost:3000',
+	// baseURL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL ?? 'http://localhost:3000',
 });
 
 export const { signIn, signUp, useSession, signOut } = authClient;
