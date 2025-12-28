d59c5220d11191f79f1e53b690fea6841a086def
aejmcclelland <aejmcclelland@googlemail.com>
2025-12-28 00:14:47 +0000

    Swapped position of label and urlon qrform component

--- src/components/qr/QrForm.tsx
+++ src/components/qr/QrForm.tsx
@@ -16,14 +16,6 @@ export function QrForm({
 	return (
 		<>
 			<fieldset className='fieldset rounded-box w-full'>
-				<label className='label mb-1'>Label (optional)</label>
-				<input
-					type='text'
-					className='input w-full'
-					value={label}
-					onChange={(e) => onLabelChange(e.target.value)}
-					placeholder='e.g. Personal, Document link, etc.'
-				/>
 				<label className='label mt-1 mb-1'>Target URL</label>
 				<input
 					type='url'
@@ -31,6 +23,14 @@ export function QrForm({
 					value={value}
 					onChange={(e) => onValueChange(e.target.value)}
 					placeholder='Paste your link here'
+				/>
+				<label className='label mb-1'>Label (optional)</label>
+				<input
+					type='text'
+					className='input w-full'
+					value={label}
+					onChange={(e) => onLabelChange(e.target.value)}
+					placeholder='e.g. Personal, Document link, etc.'
 				/>
 			</fieldset>
 		</>
